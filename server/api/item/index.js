40a2f4d9c61c7e3c31ed7a539d1312b5dc75a924
Donal Spring <donalspring@gmail.com>
2016-06-27 21:36:41 +0100

    FIX refactor break on api name

--- server/api/item/index.js
+++ server/api/item/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./thing.controller.js');
+var controller = require('./item.controller.js');
 
 var router = express.Router();
 
