6a2e6f45885c2c3f510315db7240c43035b3b5ab
Sam Marland <sam@uk.ibm.com>
2016-07-04 19:43:41 +0100

    Fixed an error deleting

--- client/js/views/todo-view.js
+++ client/js/views/todo-view.js
@@ -117,7 +117,6 @@ var app = app || {};
 
 		// Remove the item, destroy the model from *localStorage* and delete its view.
 		clear: function () {
-      this.model.set({'idAttribute': this.model.get('_id'), 'id' : this.model.get('_id') });
 			this.model.destroy();
 		}
 	});
